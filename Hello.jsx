934764f5f714ee9314dc017738a26d035f6492b2
mrin2810 <mrinmayeerane2810@gmail.com>
2022-04-02 00:15:30 +0530

    1 file modified

--- Hello.jsx
+++ Hello.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default ({ name }) => {
+  const [userData, setUserData] = useState([]);
   useEffect(() => {
     const token = sessionStorage.getItem('token');
     const apiUrl = 'https://reqres.in/api/unknown';
@@ -18,10 +19,35 @@ export default ({ name }) => {
       const data = await fetch(apiUrl, requestOptions);
       const res = await data.json();
       console.log(res.data);
+      setUserData(res.data);
     };
 
     fetchUrl();
   }, []);
 
-  return <h1>Hello {name}!</h1>;
+  return (
+    <React.Fragment>
+      <h1>Hello {name}!! </h1>
+      {userData.length && (
+        <table border={1} cellPadding={3} cellSpacing={0}>
+          <tr>
+            <th>Name</th>
+            <th>Year</th>
+          </tr>
+          {userData.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td style={{ color: user.color, fontWeight: bold }}>
+                  {user.name}
+                </td>
+                <td style={{ color: user.color, fontWeight: bold }}>
+                  {user.year}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      )}
+    </React.Fragment>
+  );
 };
